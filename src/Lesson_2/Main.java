fbd3af64d4d75650437c94331306ad0b9ba1be6b
EkaterinaShkrabova <kateshkrabova08@gmail.com>
2025-11-21 19:33:00 +0300

    add lesson_2

--- src/Lesson_2/Main.java
+++ src/Lesson_2/Main.java
@@ -1,7 +1,40 @@
 package Lesson_2;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 8;
+        int b = 1;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 5;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
 
     }
+
 }
