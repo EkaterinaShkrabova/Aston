3322cc1904d7a3fbb9f11bd66ca62e1cc9aaff82
EkaterinaShkrabova <kateshkrabova08@gmail.com>
2025-12-05 12:33:10 +0300

    lesson_4

---
+++ src/Lesson_2/ArrayProcessor.java
@@ -0,0 +1,27 @@
+package Lesson_2;
+
+public class ArrayProcessor {
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4.");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Массив должен быть размером 4x4.");
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Некорректные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+}
--- src/Lesson_2/Main.java
+++ src/Lesson_2/Main.java
@@ -2,6 +2,25 @@ package Lesson_2;
 
 public class Main {
     static void main() {
+        String[][] array = {
+                {"1", "8", "5", "3"},
+                {"8", "15", "21", "11"},
+                {"30", "21", "9", "1"},
+                {"21", "14", "21", "6"}
+        };
+        try {
+            int result = ArrayProcessor.processArray(array);
+            System.out.println("Сумма элементов: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
 
+        try {
+            array[5][0] = "test";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Обнаружена попытка обращения за границы массива: " + e.getMessage());
+        }
     }
-}
+ }
